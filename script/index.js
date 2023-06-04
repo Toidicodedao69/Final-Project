bc54790992d5e4fdba767d3643873cac4a698b16
pkduongsu <pkduongsu@gmail.com>
2023-06-04 18:09:24 +1000

    Fix Zoom and section distances

--- script/index.js
+++ script/index.js
@@ -31,7 +31,9 @@ function init() {
     }
     
     let zoom = d3.zoom()
-                .on('zoom', handleZoom);
+                .on('zoom', handleZoom)
+                .translateExtent([[0, 0], [w, h]])
+                .scaleExtent([1, 5]); //limits the zoom range
     
     d3.select('svg')
         .call(zoom);
